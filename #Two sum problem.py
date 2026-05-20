9e5c6fb7aa459c4dad62e4755210cbafd127ecc3
Aleeza Ali <aleezaali@mac.home>
2026-05-19 22:05:37 -0400

    Two sum problem

---
+++ #Two sum problem.py
@@ -0,0 +1,18 @@
+#Two sum problem
+
+def twoSum(nums, target):
+    seen = {}
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+
+        if complement in seen:
+            return [seen[complement], i]
+
+        seen[nums[i]] = i
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print(twoSum(nums, target))
